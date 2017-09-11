4a0819df96f1153958c76f903e809e38e15d7483
TheAdnan <adnankicin92@gmail.com>
2017-09-11 18:08:26 +0200

    downloader.js fixed, immediate download instead of opening in new tab

--- downloader.js
+++ downloader.js
@@ -1,18 +1,8 @@
-function execute(){
-	var src = document.querySelectorAll('img[srcset]')[0].src;
 
-    var img = document.createElement('img');
-    img.src = src; 
 
-    var a = document.createElement('a');
-    a.href = img.src;
-    a.download = "";
-
-    a.appendChild(img);
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+function download(){
+	return src = document.querySelectorAll('img[srcset]')[0].src;
 }
 
 
-execute();
+download();
--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 browser.browserAction.onClicked.addListener(function(tab){
-	browser.tabs.executeScript(null, {file: "downloader.js"});
+	var executing = browser.tabs.executeScript({
+	  file: "downloader.js"
+	});
+	executing.then(onExecuted, onError);
+	 
+
 });
+
+function onStartedDownload(id) {
+  console.log("Downloading...");
+}
+
+function onFailed(error) {
+  console.log("Download failed");
+}
+
+function onExecuted(result) {
+  var downloading = browser.downloads.download({
+      url : result[0] + "",
+      saveAs : true
+    });
+
+    downloading.then(onStartedDownload, onFailed);
+}
+
+function onError(error) {
+  console.log("Cant open downloader.js");
+}
 
 
