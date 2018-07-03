e4b0c84057f6a0aa4571b0a6d0a1bfefc417945d
TheAdnan <adnankicin92@gmail.com>
2018-07-03 16:40:14 +0200

    added instagram downloader in context menu

--- downloader.js
+++ downloader.js
@@ -1,8 +1,8 @@
-
-
+var src;
+document.onmousedown = function (event) {
+    src = event.target.parentNode.children[0].firstChild.attributes.src.value;
+};
 function download(){
-	return src = document.querySelectorAll('img[srcset]')[0].src;
+	return src;
 }
-
-
 download();
--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-browser.browserAction.onClicked.addListener(function(tab){
-	var executing = browser.tabs.executeScript({
-	  file: "downloader.js"
-	});
-	executing.then(onExecuted, onError);
-	 
-
+browser.contextMenus.create({
+    id: "download-ig-photo",
+    title: "Download photo from Instagram",
+    contexts: ["page"]
 });
 
-function onStartedDownload(id) {
-  return true;
-}
+browser.contextMenus.onClicked.addListener((info) => {
+    if (info.menuItemId === "download-ig-photo") {
+        browser.tabs.executeScript({
+            file: "downloader.js"
+        }).then((results) => {
+            onExecuted(results);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+});
 
-function onFailed(error) {
-  return false;
-}
 
 function onExecuted(result) {
   var downloading = browser.downloads.download({
-      url : result[0] + "",
+      url : result[0],
       saveAs : true
     });
-
-    downloading.then(onStartedDownload, onFailed);
+    downloading.then(onStartedDownload, function () {
+        return false;
+    });
 }
 
-function onError(error) {
-  return false;
+function onStartedDownload(id) {
+    return true;
 }
-
-
